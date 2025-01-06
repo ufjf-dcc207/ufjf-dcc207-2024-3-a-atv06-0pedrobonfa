ca47e3bba2935cf65d0c0ded5bde3c01422bbc20
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-01-06 20:23:58 -0300

    adiciona eventos

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,18 +1,34 @@
-import "./Emoji.css"
+import "./Emoji.css";
 
-const EMOJIS = new Map<String, String>(
-
-    [
-        ["happy","🙂"],
-        ["sick","🤮"],
-        ["dead","😵"],
-        
-    ]
-);
+const EMOJIS = new Map<String, String>([
+  ["happy", "🙂"],
+  ["sick", "🤮"],
+  ["dead", "😵"],
+]);
 //EMOJIS.set("happy","🙂");
 
+export default function Emoji() {
+  let situacao = "dead";
 
-export default function Emoji (){
-    let situacao = "Dead";
-    return <div className="emoji">{EMOJIS.get(situacao) || '🫥' }</div>;
+  function toHappy()
+  {
+    console.log(`toHappy()! ${situacao}`);
+    situacao = "happy";
+  }
+
+  function toDead()
+  {
+    console.log(`toDead()! ${situacao}`);
+    situacao = "dead";
+  }
+
+  return (
+    <div className="emoji">
+      <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
+      <div className="acoes">
+        <button onClick={toHappy}>Vivo</button>
+        <button onClick={toDead}>Morto</button>
+      </div>
+    </div>
+  );
 }
