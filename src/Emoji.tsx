396f7c1ffb6640b262f3c233ada9a5c2c8e22622
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-01-06 20:04:28 -0300

    adiciona redesenho condicional

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,5 +1,18 @@
 import "./Emoji.css"
 
+const EMOJIS = new Map<String, String>(
+
+    [
+        ["happy","🙂"],
+        ["sick","🤮"],
+        ["dead","😵"],
+        
+    ]
+);
+//EMOJIS.set("happy","🙂");
+
+
 export default function Emoji (){
-    return <div className="emoji">🙂</div>;
+    let situacao = "Dead";
+    return <div className="emoji">{EMOJIS.get(situacao) || '🫥' }</div>;
 }
